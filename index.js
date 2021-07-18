dd5b72d134ec1b96b349892511e49c7a6dbe082b
Joe Dietrich <joe@joedietrich.net>
2021-07-17 21:53:37 -0400

    commit tree cbb044512b47eba4a9ccb29d7db2f4e1bef070e4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": 'application/json',
+      "Accept": 'application/json',
+    },
+    body: JSON.stringify({ name, email })
+  }).then(res => res.json())
+    .then(data => {
+      const id = data.id;
+      const docBody = document.querySelector('body');
+      const newIdElement = document.createElement('p');
+      newIdElement.innerHTML = id;
+      docBody.appendChild(newIdElement);
+    })
+    .catch(err => {
+      const message = err.message;
+      const docBody = document.querySelector('body');
+      const newErrorElement = document.createElement('p');
+      newErrorElement.innerHTML = message;
+      docBody.appendChild(newErrorElement)
+    })
+}
